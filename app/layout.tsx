dc51b7b80dbf7d19bd077d6ebfc2e56c78b7fbb7
Jlvieira0909 <lvjohn09@gmail.com>
2026-04-10 15:19:53 -0300

    minor fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "Sizebay Virtual Try On",
   description: "Sizebay Virtual Try On",
 };
 
